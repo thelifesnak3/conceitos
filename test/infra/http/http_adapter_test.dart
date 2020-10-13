e2cd8195c58ed532f0f46bbbab374de6efbcb4eb
felipedev <felipe.silva1kb@gmail.com>
2020-10-13 19:28:38 -0300

    ensure HttpAdapter throws Forbidden at status 403

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -102,12 +102,20 @@ void main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
-    test('Should return UnauthorizedError if post returns 401', () async {
+    test('Should return Unauthorized if post returns 401', () async {
       mockResponse(401);
 
       final future = sut.request(url: url, method: 'post');
 
       expect(future, throwsA(HttpError.unauthorized));
+    });
+
+    test('Should return Forbidden if post returns 403', () async {
+      mockResponse(403);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.forbidden));
     });
 
     test('Should return ServerError if post returns 500', () async {
