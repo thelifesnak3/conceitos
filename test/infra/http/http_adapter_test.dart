1375d8963a8fecdfefe241d8062b54f79596810d
felipedev <felipe.silva1kb@gmail.com>
2020-10-13 19:41:44 -0300

    ensure HttpAdapter throws ServerError if post throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -38,6 +38,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
         mockResponse(200);
     });
@@ -142,5 +146,12 @@ void main() {
       expect(future, throwsA(HttpError.serverError));
     });
 
+    test('Should return ServerError if post throws', () async {
+      mockError();
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
   });
 }
