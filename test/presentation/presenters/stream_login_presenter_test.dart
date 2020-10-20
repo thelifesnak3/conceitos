2132d45e4e29dcabb3c5e69c8d9fa62ea0dffb91
felipedev <felipe.silva1kb@gmail.com>
2020-10-19 21:29:12 -0300

    ensure StreamLoginPresent emit null if email is valid

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -44,4 +44,12 @@ void main() {
     sut.validateEmail(email);
     sut.validateEmail(email);
   });
+
+  test('Shoul emit nul if validateEmail succeeds', () {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.isFormValidStream.listen(expectAsync1((isValid) => expect(isValid, false)));
+
+    sut.validateEmail(email);
+    sut.validateEmail(email);
+  });
 }
