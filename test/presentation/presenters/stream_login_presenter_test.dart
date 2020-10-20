21d95eae3dd75cf9b582d7f4e3ae7c6bf3d3036c
felipedev <felipe.silva1kb@gmail.com>
2020-10-19 21:49:52 -0300

    ensure StreamLoginPresenter emits isFormValid true when form is valid

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -89,4 +89,14 @@ void main() {
     sut.validateEmail(email);
     sut.validatePassword(password);
   });
+
+  test('Shoul isFormValid true if emailErrorStream and passwordErrorStream is null', () async {
+    sut.emailErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    sut.passwordErrorStream.listen(expectAsync1((error) => expect(error, null)));
+    expectLater(sut.isFormValidStream, emitsInOrder([false, true]));
+
+    sut.validateEmail(email);
+    await Future.delayed(Duration.zero);
+    sut.validatePassword(password);
+  });
 }
