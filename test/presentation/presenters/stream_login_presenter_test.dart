76e7c30bfb9a874f4a586c9da1108eb529708101
felipedev <felipe.silva1kb@gmail.com>
2020-10-19 21:08:44 -0300

    create PostExpectation and add some helpers

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -36,10 +36,21 @@ void main() {
   ValidationSpy validation;
   String email; 
 
+  PostExpectation mockValidationCall(String field) => 
+    when(validation.validate(
+      field: field == null ? anyNamed('field') : field,
+      value: anyNamed('value')
+    ));
+
+  void mockValidation({String field, String value}) {
+    mockValidationCall(field).thenReturn(value);
+  }
+
   setUp(() {
     validation = ValidationSpy();
     sut = StreamLoginPresenter(validation: validation);
     email = faker.internet.email();
+    mockValidation();
   });
 
   test('Shoul call Validation with correct email', () {
@@ -49,8 +60,7 @@ void main() {
   });
 
   test('Shoul emit email error if validateEmail fails', () {
-    when(validation.validate(field: anyNamed('field'), value: anyNamed('value')))
-      .thenReturn('error');
+      mockValidation(value: 'error');
 
     expectLater(sut.emailErrorStream, emits('error'));
 
